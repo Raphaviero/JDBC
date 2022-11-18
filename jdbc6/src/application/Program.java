3cc39fb2c31bc6bc33133d0e8b0d971ed11e8f8c
Raphael Viero <109819996+Raphaviero@users.noreply.github.com>
2022-11-18 11:22:53 -0300

    Database Transactions
    Working with defensive programming in the Bank using: setAutoCommit(); commit(); and rollback.

---
+++ jdbc6/src/application/Program.java
@@ -0,0 +1,57 @@
+package application;
+
+/*Ocasionando um erro para observar 
+a açao de rollback durante uma TRANSAÇÃO DE VALORES no banco.
+*/
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+import db.dbException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		Statement st = null;
+
+		try {
+			conn = DB.getConnection();
+			
+			conn.setAutoCommit(false);
+			
+			st = conn.createStatement();
+			
+
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+			
+			/*int x = 1;
+			if (x < 2) {
+				throw new SQLException("Fake Error");
+			}*/
+			
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2 ");
+			
+			
+			conn.commit();
+			
+			System.out.println("rows1 : " + rows1);
+			System.out.println("rows1 : " + rows2);
+		} 
+		catch (SQLException e) {
+			try {
+				conn.rollback();
+				throw new dbException("Transaction rolled back! Caused by " + e.getMessage());
+			} 
+			catch (SQLException e1) {
+				throw new dbException("Error trying to rollback! Caused by : " + e.getMessage());
+			}
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+
+	}
+}
